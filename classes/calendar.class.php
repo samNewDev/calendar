d8bf0d07a3e51e288bd60bbe06e70858c05dc3d8
samNewDev <samirchaouch21@outlook.fr>
2020-05-03 12:44:32 +0200

    initial commit 'adding weeksDefaultNum'

---
+++ classes/calendar.class.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ *
+ */
+class Calendar
+{
+  public $weeksDefaultNum = 6;
+  public $days = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
+  private $months = ['Janvier', 'Fevrier', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Aout', 'Septembre', 'Octobre', 'Novembre', 'Decembre'];
+  private $month;
+  private $year;
+
+  function __construct(?int $month = null, ?int $year = null) {
+    if ($month == null) {
+      $month = intval(date('m'));
+    }
+    if ($year == null) {
+      $year = intval(date('Y'));
+    }
+    if ($month > 12 || $month < 1) {
+      $month = $month % 12;
+    }
+    $this->month = $month;
+    $this->year = $year;
+  }
+
+  public function displayDate() : string {
+    return $this->months[$this->month - 1] . " " . $this->year;
+  }
+
+  public function firstDayOfTheWeek() : DateTime {
+    return new DateTime("$this->year-$this->month-01");
+  }
+
+  public function getWeeksNum() : int {
+    $start = $this->firstDayOfTheWeek();
+    $end = (clone $start)->modify("+1 month -1 day");
+    return intval($end->format('W')) - intval($start->format('W')) + 1;
+  }
+
+
+}
---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+include "includes/myautoload.inc.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>My Calendar</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="style/style.css">
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-md navbar-dark bg-success">
+        <a class="navbar-brand" href="index.php">My Calendar</a>
+    </nav>
+    <?php
+      $month = new Calendar($_GET['month'] ?? null, $_GET['year'] ?? null);
+      $firstDay = $month->firstDayOfTheWeek()->modify('last monday');
+    ?>
+    <h1><?= $month->displayDate() ?></h1>
+    <table class="table table-bordered table-success calendar_table">
+      <?php for($i=0; $i<6; $i++) : ?>
+      <tr>
+        <?php foreach ($month->days as $key => $day): ?>
+        <td class="calendar_weeks calendar_<?=$month->getWeeksNum()?>weeks">
+          <?php if($i == 0) : ?>
+            <?= $day . '<br>'; ?>
+          <?php endif; ?>
+          <?php echo intval($firstDay->format('d')); ?>
+        </td>
+        <?php endforeach; ?>
+      </tr>
+    <?php endfor; ?>
+    </table>
+  </body>
+</html>
